7aeee6b5d9e4e75739a15527475f1ceb5ae86681
cmk5378 <cmk5378@naver.com>
2021-05-27 13:49:06 +0900

    버튼, 메인화면 수정

--- src/main/resources/static/js/commercial.js
+++ src/main/resources/static/js/commercial.js
@@ -367,37 +367,37 @@ var geocoder = new kakao.maps.services.Geocoder();
 function addressMark(coronaList, hospitalName){
 	removeSearchMarkers();
 	for(var idx=0; idx<coronaList.length; idx++){
+		console.log("병원이름"+hospitalName[idx])
+        
 		// 주소로 좌표를 검색합니다
 		geocoder.addressSearch(coronaList[idx], function(result, status) {
 			var imageSrc = './img/hospital.png', // 마커이미지의 주소입니다    
-		    imageSize = new kakao.maps.Size(40, 40), // 마커이미지의 크기입니다
-		    imageOption = {offset: new kakao.maps.Point(20, 69)}; // 마커이미지의 옵션입니다. 마커의 좌표와 일치시킬 이미지 안에서의 좌표를 설정합니다.
+			imageSize = new kakao.maps.Size(40, 40), // 마커이미지의 크기입니다
+			imageOption = {offset: new kakao.maps.Point(20, 69)}; // 마커이미지의 옵션입니다. 마커의 좌표와 일치시킬 이미지 안에서의 좌표를 설정합니다.
 			var markerImage = new kakao.maps.MarkerImage(imageSrc, imageSize, imageOption);
 			
 			// 정상적으로 검색이 완료됐으면 
-		     if (status === kakao.maps.services.Status.OK) {
-		
-		        var coords = new kakao.maps.LatLng(result[0].y, result[0].x);
-		        
-		        // 결과값으로 받은 위치를 마커로 표시합니다
-		        var marker = new kakao.maps.Marker({
-		            map: map,
-		            position: coords,
-		            image: markerImage // 마커이미지 설정 
-		        });
-
-		        // 인포윈도우로 장소에 대한 설명을 표시합니다
+			if (status === kakao.maps.services.Status.OK) {
+				
+				var coords = new kakao.maps.LatLng(result[0].y, result[0].x);
+				
+				// 결과값으로 받은 위치를 마커로 표시합니다
+				var marker = new kakao.maps.Marker({
+					map: map,
+					position: coords,
+					image: markerImage // 마커이미지 설정 
+				});
+				
+				// 인포윈도우로 장소에 대한 설명을 표시합니다
 //		        var infowindow = new kakao.maps.InfoWindow({
 //		            content: `<div style="width:150px;text-align:center;padding:6px 0;">${hospitalName[idx]}</div>`
-//		            //content: '<div style="width:150px;text-align:center;padding:6px 0;">🚑선별진료소🚑</div>'
 //		        });
-//		        infowindow.open(map, marker);
-		
-		        // 지도의 중심을 결과값으로 받은 위치로 이동시킵니다
-		        map.setCenter(coords);
-		        map.setLevel(7);
-		    } 
+//				infowindow.open(map, marker);
+				
+				// 지도의 중심을 결과값으로 받은 위치로 이동시킵니다
+				map.setCenter(coords);
+				map.setLevel(7);
+			} 
 		});
 	}
 }
-
